9e3a9aeb06b4d08d2a22f5efc45f27c5f8611d35
Aaron Peron <aaron.peron@yahoo.com>
2015-01-08 14:03:07 -0800

    on track

--- public_html/script.js
+++ public_html/script.js
@@ -6,5 +6,19 @@
 
 
 $("document").ready(function() {
-    $(".practice").css("background-color", "green"),
+    $("practice").css({"background-color": "purple", "color": "whiht"});
+    
+    $("p span:first-child").css("background-color", "orange");
+    
+    $("div p:last-child").css("background", "pink");
+    
+    $("div:nth-child(1)").css("background", "cyan");
+    
+    $("span:eq(9)").css("background-color", "purple");
+    
+    $("h4:gt(0)").css("background-color", "green");
+    
+    $("h4:lt(2)").css("background-color", "olive");
+    
+    $("")
 });
