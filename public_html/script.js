9bcf65d5356af461c931fd6979801a62302d3d8a
Aaron Peron <aaron.peron@yahoo.com>
2015-01-13 14:03:19 -0800

    still not working

--- public_html/script.js
+++ public_html/script.js
@@ -33,16 +33,85 @@ $("document").ready(function() {
             $('#clickToHide').fadeTn(2000);
             $(this).val('Delete Text');
         }
+        
+    $('#oneButton').bind('click', alertButtonClick);
+    
+    $('#textBox1').bind('blur', onBlurEvent)
+                  .bind('focus', onFocusEvent)
+                  .bind('onmousedownn', onMDownEvent)
+                  .bind('onmouseup', onMUpEvent)
+                  .bimd('charge', onChangeEvent);
+          
+    $(window).resize(resizedWindow);
+
+    $('#logo').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+    $('#twoButton').bind('dblclick', dblClickedMe);
+
+    $("form").submit(function() { alert("Submit button clicked") });
+    
+    $('#threeButton').bind('click', unbindLogo);
+    
+    $('#theBody').bind('keyup', checkKeyPressed).bind('mousemove',
+    theMouseMoved).click(event,eventTriggered);
+    
+    $('#replaceWHtml').bind('click', replaceWHtml);
 });
 
 function mouseOverMe(){
     $("p").html("Testing");
-};
+}
 
 function mouseOnMe(){
     $('hl').html("It's Free!!!");
-};
+}
 
 function mouseClick(){
     $("p").html("I am still here.");
-};
+}
+
+function alertButtonClick(){
+    alert("There was a button clicked");
+}
+
+function onBlurEvent(){
+    $("#second").html("Gone");
+}
+
+function onFocusEvent(){
+    $("#second").html("Gone");
+}
+
+function onMDownEvent(){
+    $("#second").html("Gone");
+}
+
+function onMUpEvent(){
+    $("#second").html("Here");
+}
+
+function onChangeEvent(){
+    $("#second").html("Change");
+}
+
+function dblClickedMe(){
+    $("#second").html("see this");
+}
+
+function unbindLogo(){
+    $("#logo").unbind('mouseover', mouseOverMe).unbind('mouseout', mouseOutMe);
+}
+
+function checkKeyPressed(event){
+    $("#fifth").text(String.fromCharCode(event.keyCode));
+}
+
+function theMouseMoved(event){
+    $("#seventh").html(event.screenX);
+    $("#ninth").html(event.screenY);
+}
+
+function eventTriggered(event){
+    $("#tenth").html(event.target.nodeName);
+    $("#eleventh").html(event.timeStomp);
+}
